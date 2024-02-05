fb926ceb0c97e70911401377ce2f6b8371e5b594
banarin <banarin60@gmail.com>
2024-02-05 13:04:04 +0000

    5fev 2 pharma mackette integration 2

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path("index", views.index_page, name="index"),
+    path("contact", views.contact_page, name="contact_page"),
+    path("produits", views.produits_page, name="produits_page"),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 def index_page(request):
     print("test 1")
     return render(request, 'app/home.html')
+
+def contact_page(request):
+    print("contact")
+    return render(request, 'app/contact.html')
+def produits_page(request):
+    print("produit")
+    return render(request, 'app/produits.html')
